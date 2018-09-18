d8fc5bc121c481dc12d8f52de9942d483aac4da1
Ni jie <nij@gjmetal.com>
2018-09-18 17:16:24 +0800

    1

--- src/main/java/my/config/server/ConfigServerApplication.java
+++ src/main/java/my/config/server/ConfigServerApplication.java
@@ -8,25 +8,24 @@ import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfig
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-
+@EnableEurekaClient
 @EnableConfigServer
-@SpringBootApplication(exclude={EndpointAutoConfiguration.class,JpaRepositoriesAutoConfiguration.class,DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
-public class ConfigServerApplication  {
-	
-	
-	/*@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		builder.sources(this.getClass());
-		return super.configure(builder);
-	}
-	*/
-	
-	public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(ConfigServerApplication.class);
-        app.setBannerMode(Banner.Mode.OFF);
-        app.run(args);
-    }
+@SpringBootApplication(exclude = { EndpointAutoConfiguration.class, JpaRepositoriesAutoConfiguration.class,
+		DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
+public class ConfigServerApplication {
 
-	
+	/*
+	 * @Override protected SpringApplicationBuilder
+	 * configure(SpringApplicationBuilder builder) {
+	 * builder.sources(this.getClass()); return super.configure(builder); }
+	 */
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(ConfigServerApplication.class);
+		app.setBannerMode(Banner.Mode.OFF);
+		app.run(args);
+	}
+
 }
