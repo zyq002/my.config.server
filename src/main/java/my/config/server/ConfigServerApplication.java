554019da0eacf58274314c92f630dd2382d27e7a
Ni jie <nij@gjmetal.com>
2018-09-19 14:38:52 +0800

    7

--- src/main/java/my/config/server/ConfigServerApplication.java
+++ src/main/java/my/config/server/ConfigServerApplication.java
@@ -7,20 +7,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @EnableConfigServer
-@SpringBootApplication(exclude = { EndpointAutoConfiguration.class, JpaRepositoriesAutoConfiguration.class,
-		DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
-public class ConfigServerApplication {
+@SpringBootApplication
+public class ConfigServerApplication extends SpringBootServletInitializer {
 
-	/*
-	 * @Override protected SpringApplicationBuilder
-	 * configure(SpringApplicationBuilder builder) {
-	 * builder.sources(this.getClass()); return super.configure(builder); }
-	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		builder.sources(this.getClass());
+		return super.configure(builder);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(ConfigServerApplication.class);
