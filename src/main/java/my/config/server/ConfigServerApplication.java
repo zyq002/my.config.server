29ec83164fc5c339efa7f8ca798211a3c4577948
Ni jie <nij@gjmetal.com>
2018-09-20 17:56:33 +0800

    1

--- src/main/java/my/config/server/ConfigServerApplication.java
+++ src/main/java/my/config/server/ConfigServerApplication.java
@@ -2,11 +2,7 @@ package my.config.server;
 
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.EndpointAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -20,6 +16,7 @@ public class ConfigServerApplication extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		builder.sources(this.getClass());
+ 
 		return super.configure(builder);
 	}
 
