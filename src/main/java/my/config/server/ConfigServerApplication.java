d461a7653aa6f2dc421a21786c7f095bcf397d88
nijie <nijie@nijie-PC>
2018-11-06 18:24:53 +0800

    1

--- src/main/java/my/config/server/ConfigServerApplication.java
+++ src/main/java/my/config/server/ConfigServerApplication.java
@@ -1,6 +1,5 @@
 package my.config.server;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
